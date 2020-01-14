f15e06e3dc9543d5443ff8fc853f113842cf3a29
Marijana <mm20170100@student.fon.bg.ac.rs>
2020-01-14 14:59:22 +0100

    final

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-function submit() {
-    var broj1 = parseInt(document.getElementsById("broj1").value);
-    var broj2 = parseInt(document.getElementsById("broj2").value);
+function Submit() {
+    var broj1 = parseInt(document.getElementById("broj1").value);
+    var broj2 = parseInt(document.getElementById("broj2").value);
 
     var operacija = document.getElementById('operacije').value;
 
+    var rezultat;
+
     if (operacija === "sabiranje") {
+        rezultat = broj1 + broj2;
         document.getElementById("rezultat").value = broj1 + broj2;
     }
     if (operacija === "oduzimanje") {
+        rezultat = broj1 - broj2;
         document.getElementById("rezultat").value = broj1 - broj2;
     }
     if (operacija === "mnozenje") {
+        rezultat = broj1 * broj2;
         document.getElementById("rezultat").value = broj1 * broj2;
     }
     if (operacija === "deljenje") {
+        rezultat = broj1 / broj2;
         document.getElementById("rezultat").value = broj1 / broj2;
     }
+
+    console.log(rezultat)
 }
